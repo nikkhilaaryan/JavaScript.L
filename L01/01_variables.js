636d6b75a954f4c28cc89db0955ec4a2cf197119
ARYAN RAJ <nikhilaryan0928@gmail.com>
2025-02-22 14:20:14 +0000

    Learned variables, let, and const in JavaScript

---
+++ L01/01_variables.js
@@ -0,0 +1,17 @@
+const account_ID = 908727
+let account_Email = "javaScript@hotmail.com"
+var account_Password = "4589"
+account_Address = "Script"
+let account_State;
+
+
+account_Email = "ScriptJava@hotmail.com"
+account_Password = "2789"
+account_Address = "Java"
+
+console.table([account_ID, account_Email, account_Password, account_Address, account_State])
+
+/*
+Prefer not to use var instead of that use let 
+because of the issue in block scope and functionl scope.
+*/
