eab57a5c2aa897f613748a707359fa02301fb15d
ARYAN RAJ <nikhilaryan0928@gmail.com>
2025-02-22 15:07:43 +0000

    JavaScript Data Types

---
+++ L01/02_dataTypes.js
@@ -0,0 +1,20 @@
+"use strict"; // treat all JS code as newer version
+console.log("JavaScript")
+let name = "Java"
+let age = "26"
+let isLoggedIn = false
+let state;
+
+// alert(8/2) // as we are using nodejs, not browser
+//number => 2 to the power 53 
+// bigint
+// strings = ""
+//boolean => true/false 
+// null => standalone value
+// undefined => value not assigned 
+// symbol => unique 
+
+// object
+console.log(typeof "JavaScript");
+console.log(typeof null); // null is an object
+
