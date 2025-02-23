548316f31bfad31064ae20421fa1d766db7f0558
ARYAN RAJ <nikhilaryan0928@gmail.com>
2025-02-23 12:19:13 +0000

    conversionOperations

--- L01/03_conversionOperations.js
+++ L01/03_conversionOperations.js
@@ -1,10 +1,10 @@
 let score = "56xyz";
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
 let valueInNumber = Number(score);
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 //"56" => 56
 //"56xyz" => NaN (Not a Number)
@@ -13,16 +13,54 @@ console.log(valueInNumber);
 
 let isLoggedIn = 1;
 let booleanIsLoggedIn = Boolean(isLoggedIn);
-console.log(isLoggedIn);
-console.log(booleanIsLoggedIn);
+// console.log(isLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 //1 => true; 0=> false;
 //"" => false; "Java" => true 
 
 let someNumber = 43;
 let stringSomeNumber = String(someNumber);
-console.log(stringSomeNumber);
-console.log(typeof stringSomeNumber);
+// console.log(stringSomeNumber);
+// console.log(typeof stringSomeNumber);
+
+//                                                       *Operations*
+
+let value = 5;
+let negValue = -value;
+console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2%2);
+// console.log(2**2); // 2 to the power 2
+// console.log(2**3); // 2 to the power 3
+
+let str1 = "Java";
+let srt2 = "Script";
+let str3 = str1 + srt2;
+// console.log(str3);
+
+// console.log(1 + "2");
+// console.log("1" + 2);
+// console.log("1" + 3 + 3);
+// console.log(1 + 3 + "3");
+
+console.log(+true); // output : 1
+console.log(+""); // output : 0
+
+
+
+
+
+
+
+
+
+
+
 
 
 
