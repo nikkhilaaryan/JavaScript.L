4b7d351820bccecc8a4e7c567f4f8be587acb993
ARYAN RAJ <nikhilaryan0928@gmail.com>
2025-02-23 07:28:25 +0000

    conversionOperations

---
+++ L01/03_conversionOperations.js
@@ -0,0 +1,33 @@
+let score = "56xyz";
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//"56" => 56
+//"56xyz" => NaN (Not a Number)
+//true => 1 
+//false => 0
+
+let isLoggedIn = 1;
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+//1 => true; 0=> false;
+//"" => false; "Java" => true 
+
+let someNumber = 43;
+let stringSomeNumber = String(someNumber);
+console.log(stringSomeNumber);
+console.log(typeof stringSomeNumber);
+
+
+
+
+
+
+
+
