605bf29628bc8aaa4f48d515bd34d964b4d7e05c
ARYAN RAJ <nikhilaryan0928@gmail.com>
2025-04-24 06:35:05 +0000

    truthy.falsy

---
+++ L04/03_truthy.js
@@ -0,0 +1,39 @@
+// const userEmail = "script@java";
+
+// if (userEmail) {
+//     console.log("UserEmail Confirmed");
+    
+// }else {
+//     console.log("Unauthorized Access");
+    
+// }
+
+//falsy values;
+//false, 0, -0, BigInt 0n, " ", null, undefined, Nan(Not a number).
+
+//truthy value;
+//"0", 'faslse', " ", [], {}, function(){},
+
+// if (userEmail.length === 0) {
+//     console.log("array is empty");
+    
+// }
+
+
+// const emptyObject = {}
+
+// if (Object.keys(emptyObject).length === 0) {
+//     console.log("Object Empty");
+    
+// }
+
+
+//Nullish Coalescing Operator (??): null and undefined
+
+let val1;
+// val1 = 5 ?? 10;
+// val1 = null ?? 10;
+var1  = undefined ?? 10; 
+console.log(val1);
+console.log(var1 );
+
