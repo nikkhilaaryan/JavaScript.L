37705893499422fe1a5d109b60079ce5078c1a38
ARYAN RAJ <nikhilaryan0928@gmail.com>
2025-05-02 17:32:09 +0000

    loops discussion

---
+++ L05/07_iteration.js
@@ -0,0 +1,10 @@
+const isNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// const newNums = myNumers.map( (num) => { return num + 10})
+
+const newNums = isNumbers
+                .map((num) => num * 10 )
+                .map( (num) => num + 1)
+                .filter( (num) => num >= 40)
+
+console.log(newNums);
